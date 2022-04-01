28b82acfb870e5204129ad2eefa624a1a4c92d71
Jonathan Salmon <jonathan.salmon@hotmail.co.uk>
2022-04-01 21:47:24 +0100

    Cleanup example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
-
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:windows_single_instance/windows_single_instance.dart';
 
 void main(List<String> args) async {
@@ -12,10 +9,6 @@ void main(List<String> args) async {
   });
   runApp(const MyApp());
   doWhenWindowReady(() {
-    // final initialSize = Size(600, 450);
-    // appWindow.minSize = initialSize;
-    // appWindow.size = initialSize;
-    // appWindow.alignment = Alignment.center;
     appWindow.show();
   });
 }
@@ -28,49 +21,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  bool? _isSingleInstance;
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion = await WindowsSingleInstance.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    String value = "unkown";
-    if (_isSingleInstance != null) {
-      value = _isSingleInstance! ? "Yes" : "No";
-    }
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text("Single instance: $value\n"),
+        body: const Center(
+          child: Text("Demo"),
         ),
       ),
     );
--- lib/windows_single_instance.dart
+++ lib/windows_single_instance.dart
@@ -8,120 +8,100 @@ import 'package:ffi/ffi.dart';
 import 'package:flutter/services.dart';
 import 'package:win32/win32.dart';
 
-const _pipeName = "\\\\.\\pipe\\songbookpro_instance_checkers";
-const ERROR_PIPE_CONNECTED = 0x80070217;
-// const ERROR_IO_PENDING = 0x800703E5;
-
-int _openPipe(String filename) {
-  final cPipe = filename.toNativeUtf16();
-  try {
-    return CreateFile(cPipe, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, 0);
-  } finally {
-    free(cPipe);
-  }
-}
-
-int _createPipe(String filename) {
-  final cPipe = filename.toNativeUtf16();
-  try {
-    return CreateNamedPipe(
-      cPipe,
-      PIPE_ACCESS_DUPLEX | FILE_FLAG_FIRST_PIPE_INSTANCE | FILE_FLAG_OVERLAPPED,
-      PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
-      PIPE_UNLIMITED_INSTANCES,
-      4096,
-      4096,
-      0,
-      nullptr,
-    );
-  } finally {
-    malloc.free(cPipe);
-  }
-}
-
-void _readPipe(SendPort writer, int pipeHandle) {
-  final overlap = calloc<OVERLAPPED>();
-  try {
-    while (true) {
-      while (true) {
-        ConnectNamedPipe(pipeHandle, overlap);
-        final err = GetLastError();
-        if (err == ERROR_PIPE_CONNECTED) {
-          sleep(Duration(milliseconds: 200));
-          continue;
-        } else if (err == ERROR_INVALID_HANDLE) {
-          return;
-        }
-        break;
-      }
-
-      var dataSize = 16384;
-      var data = calloc<Int8>(dataSize);
-      final numRead = calloc<Uint32>();
-      try {
-        while (GetOverlappedResult(pipeHandle, overlap, numRead, 0) == 0) {
-          sleep(Duration(milliseconds: 200));
-        }
-
-        ReadFile(pipeHandle, data, dataSize, numRead, overlap);
-        final jsonData = data.cast<Utf8>().toDartString();
-        writer.send(jsonDecode(jsonData));
-      } catch (error) {
-        stderr.writeln("[MultiInstanceHandler]: ERROR: $error");
-      } finally {
-        free(data);
-        free(numRead);
-        DisconnectNamedPipe(pipeHandle);
-      }
-    }
-  } finally {
-    free(overlap);
-  }
-}
-
-String get _lastErrToString {
-  Pointer<Utf16> errBuf = nullptr;
-  try {
-    FormatMessage(0x00000100 | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, GetLastError(), 0,
-        errBuf, 0, nullptr);
-    return errBuf.toDartString();
-  } finally {
-    free(errBuf);
-  }
-}
-
-void _writePipeData(String filename, List<String>? arguments) {
-  final pipe = _openPipe(filename);
-  final bytesString = jsonEncode(arguments ?? []);
-  final bytes = bytesString.toNativeUtf8();
-  final numWritten = malloc<Uint32>();
-  try {
-    final result = WriteFile(pipe, bytes, bytesString.length, numWritten, nullptr);
-    if (result == 0) {
-      throw _lastErrToString;
-    }
-  } finally {
-    free(numWritten);
-    free(bytes);
-    CloseHandle(pipe);
-  }
-}
-
-void _startReadPipeIsolate(SendPort writer) {
-  final pipe = _createPipe(_pipeName);
-  if (pipe == INVALID_HANDLE_VALUE) {
-    print("Pipe create failed");
-    return;
-  }
-  _readPipe(writer, pipe);
-}
-
 class WindowsSingleInstance {
   static const MethodChannel _channel = MethodChannel('windows_single_instance');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static const _pipeName = "\\\\.\\pipe\\songbookpro_instance_checkers";
+  static const ERROR_PIPE_CONNECTED = 0x80070217;
+
+  static int _openPipe(String filename) {
+    final cPipe = filename.toNativeUtf16();
+    try {
+      return CreateFile(cPipe, GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, 0);
+    } finally {
+      free(cPipe);
+    }
+  }
+
+  static int _createPipe(String filename) {
+    final cPipe = filename.toNativeUtf16();
+    try {
+      return CreateNamedPipe(
+        cPipe,
+        PIPE_ACCESS_INBOUND | FILE_FLAG_FIRST_PIPE_INSTANCE | FILE_FLAG_OVERLAPPED,
+        PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+        PIPE_UNLIMITED_INSTANCES,
+        4096,
+        4096,
+        0,
+        nullptr,
+      );
+    } finally {
+      malloc.free(cPipe);
+    }
+  }
+
+  static void _readPipe(SendPort writer, int pipeHandle) {
+    final overlap = calloc<OVERLAPPED>();
+    try {
+      while (true) {
+        while (true) {
+          ConnectNamedPipe(pipeHandle, overlap);
+          final err = GetLastError();
+          if (err == ERROR_PIPE_CONNECTED) {
+            sleep(const Duration(milliseconds: 200));
+            continue;
+          } else if (err == ERROR_INVALID_HANDLE) {
+            return;
+          }
+          break;
+        }
+
+        var dataSize = 16384;
+        var data = calloc<Int8>(dataSize);
+        final numRead = calloc<Uint32>();
+        try {
+          while (GetOverlappedResult(pipeHandle, overlap, numRead, 0) == 0) {
+            sleep(Duration(milliseconds: 200));
+          }
+
+          ReadFile(pipeHandle, data, dataSize, numRead, overlap);
+          final jsonData = data.cast<Utf8>().toDartString();
+          writer.send(jsonDecode(jsonData));
+        } catch (error) {
+          stderr.writeln("[MultiInstanceHandler]: ERROR: $error");
+        } finally {
+          free(data);
+          free(numRead);
+          DisconnectNamedPipe(pipeHandle);
+        }
+      }
+    } finally {
+      free(overlap);
+    }
+  }
+
+  static void _writePipeData(String filename, List<String>? arguments) {
+    final pipe = _openPipe(filename);
+    final bytesString = jsonEncode(arguments ?? []);
+    final bytes = bytesString.toNativeUtf8();
+    final numWritten = malloc<Uint32>();
+    try {
+      WriteFile(pipe, bytes, bytesString.length, numWritten, nullptr);
+    } finally {
+      free(numWritten);
+      free(bytes);
+      CloseHandle(pipe);
+    }
+  }
+
+  static void _startReadPipeIsolate(SendPort writer) {
+    final pipe = _createPipe(_pipeName);
+    if (pipe == INVALID_HANDLE_VALUE) {
+      print("Pipe create failed");
+      return;
+    }
+    _readPipe(writer, pipe);
   }
 
   static Future ensureSingleInstance(List<String> arguments, {Function(List<String>)? onSecondWindow}) async {
@@ -131,7 +111,7 @@ class WindowsSingleInstance {
       exit(0);
     }
 
-    // No callback so don't both starting pipe
+    // No callback so don't bother starting pipe
     if (onSecondWindow == null) {
       return;
     }
