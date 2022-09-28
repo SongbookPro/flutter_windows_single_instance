442741f8ef485c514f983135db02581f2034ba04
Osman Aslancan <osmanaslancan24@gmail.com>
2022-09-29 02:55:04 +0300

    Add Option For bringWindowToFront (#3)
    * add option
    
    * review fixes
    
    * fix mock

--- lib/src/windows_single_instance.mock.dart
+++ lib/src/windows_single_instance.mock.dart
@@ -2,7 +2,9 @@ class WindowsSingleInstance {
   WindowsSingleInstance._();
 
   static Future ensureSingleInstance(List<String> arguments, String pipeName,
-      {Function(List<String>)? onSecondWindow}) async {
-    throw UnimplementedError("windows_single_instance not supported on this platform");
+      {Function(List<String>)? onSecondWindow,
+      bool bringWindowToFront = true}) async {
+    throw UnimplementedError(
+        "windows_single_instance not supported on this platform");
   }
 }
